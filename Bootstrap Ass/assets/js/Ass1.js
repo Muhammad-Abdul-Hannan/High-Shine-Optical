72e504d8baf299be21b70ea523377389f6ac28a1
Ali Raza <meetaliraza@outlook.com>
2022-03-19 10:01:12 +0500

    Structure Updated

---
+++ Bootstrap Ass/assets/js/Ass1.js
@@ -0,0 +1,27 @@
+// Aos animation script
+AOS.init();
+    
+    
+
+// Login form script
+document.querySelector("#show-login").addEventListener("click",function(){
+    document.querySelector(".popup").classList.add("active");
+    document.querySelector(".popup").style.display="block";
+});
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+    document.querySelector(".popup").classList.remove("active");
+    document.querySelector(".popup").style.display="none";
+});
+
+
+// Navbar Scroll Script
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("menu").style.top = "0";
+    } else {
+        document.getElementById("menu").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+}
