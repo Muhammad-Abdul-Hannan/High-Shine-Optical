d1e3b4a9b7fba1c31c7f12b82f2f4e791e366491
Ali Raza <meetaliraza@outlook.com>
2022-03-26 08:56:53 +0500

    New Changes

--- Bootstrap Ass/assets/js/Ass1.js
+++ Bootstrap Ass/assets/js/Ass1.js
@@ -26,3 +26,43 @@ var currentScrollPos = window.pageYOffset;
     prevScrollpos = currentScrollPos;
 }
 
+
+/**
+   * Testimonials slider
+   */
+ new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
+
+
+  /**
+   * Preloader
+   */
+//    let preloader = select('#preloader');
+//    if (preloader) {
+//      window.addEventListener('load', () => {
+//        preloader.remove()
+//      });
+//    }
