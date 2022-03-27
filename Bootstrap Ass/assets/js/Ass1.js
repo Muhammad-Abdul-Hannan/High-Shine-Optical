8f99132f705e4efae9b02e83ebc814cf8441a6a1
Ali Raza <meetaliraza@outlook.com>
2022-03-27 09:21:23 +0500

    Changes

--- Bootstrap Ass/assets/js/Ass1.js
+++ Bootstrap Ass/assets/js/Ass1.js
@@ -1,20 +1,32 @@
 // Aos animation script
 AOS.init();
-    
-    
 
-// Login form script
-document.querySelector("#show-login").addEventListener("click",function(){
+
+window.onload = function() {
+  
+  new gnMenu( document.getElementById( 'gn-menu' ) );
+
+  Preloader();
+  swiper_user();
+  login();
+
+
+}
+
+function login(){
+  // Login form script
+  document.querySelector("#show-login").addEventListener("click",function(){
     document.querySelector(".popup").classList.add("active");
     document.querySelector(".popup").style.display="block";
-});
-document.querySelector(".popup .close-btn").addEventListener("click",function(){
+  });
+  document.querySelector(".popup .close-btn").addEventListener("click",function(){
     document.querySelector(".popup").classList.remove("active");
     document.querySelector(".popup").style.display="none";
-});
+  });
+}
 
-/** Testimonials slider */
- new Swiper('.testimonials-slider', {
+function swiper_user(){
+  new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
     autoplay: {
@@ -39,31 +51,39 @@ document.querySelector(".popup .close-btn").addEventListener("click",function(){
       }
     }
   });
+}
 
 
-// Preloader
-let preloader = document.getElementById("preloader");
-let myVar = setInterval(myTimer ,1000);
-function myTimer() {
-  preloader.style.opacity = '0';
+ 
+
+function Preloader(){
+  // Preloader
+  let preloader = document.getElementById("preloader");
+  let myVar = setInterval(myTimer ,1000);
+  function myTimer() {
+    preloader.style.opacity = '0';
+  }
+  let myVar2 = setInterval(myTimer2 ,2000);
+  function myTimer2() {
+    preloader.style.visibility = 'hidden';
+  }  
 }
-let myVar2 = setInterval(myTimer2 ,2000);
-function myTimer2() {
-  preloader.style.visibility = 'hidden';
-}
+
+
 
 
 // Scroll up button Js
-//Get the button
-var mybutton = document.getElementById("scrollToTopBtn");
-
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  //Get the button
+  var mybutton = document.getElementById("scrollToTopBtn");
 
-function scrollFunction() {
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
-}
+};
+
+
+
