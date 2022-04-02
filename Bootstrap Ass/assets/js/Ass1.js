c2ed4c201d81c60e817dcc90718f287e20c5e833
Muhammad Abdul Hannan <hannanryk2@gmail.com>
2022-04-01 23:24:13 -0700

    1

--- Bootstrap Ass/assets/js/Ass1.js
+++ Bootstrap Ass/assets/js/Ass1.js
@@ -115,3 +115,46 @@ window.onscroll = function() {
 
 
 
+// Product slider
+
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	var carousel = function() {
+		$('.featured-carousel').owlCarousel({
+	    loop:true,
+	    autoplay: true,
+	    margin:30,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    dots: true,
+	    autoplayHoverPause: true,
+	    items: 1,
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:2
+	      },
+	      1000:{
+	        items:3
+	      }
+	    }
+		});
+
+	};
+	carousel();
+
+})(jQuery);
